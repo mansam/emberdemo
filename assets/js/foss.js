83feeb9e2d9080d6097d423c031184d355f0a0e1
Samuel Lucidi <sxl6435@rit.edu>
2012-09-15 18:54:45 -0400

    More with outlets, bug fixes
    Got rid of extra Foss.initialize() that was duplicating output.
    Named base outlet for clarity's sake.
    Added a suboutlet to the about page.

--- assets/js/foss.js
+++ assets/js/foss.js
@@ -5,6 +5,7 @@ Foss = Ember.Application.create({
 Foss.ApplicationController = Ember.Controller.extend();
 Foss.IndexController = Ember.Controller.extend();
 Foss.AboutController = Ember.Controller.extend();
+Foss.DetailsController = Ember.Controller.extend();
 
 Foss.ApplicationView = Ember.View.extend({
 	templateName: 'application'
@@ -15,13 +16,16 @@ Foss.IndexView = Ember.View.extend({
 Foss.AboutView = Ember.View.extend({
 	templateName: 'about'
 });
+Foss.DetailsView = Ember.View.extend({
+	templateName: 'details'
+});
 
 Foss.Router = Ember.Router.extend({
 	root: Ember.Route.extend({
 		index: Ember.Route.extend({
 			route: '/',
 			connectOutlets: function (router, context) {
-				router.get('applicationController').connectOutlet('index');
+				router.get('applicationController').connectOutlet('top', 'index');
 			},
 			goAbout: function (router, context) {
 				router.transitionTo('about');
@@ -30,7 +34,8 @@ Foss.Router = Ember.Router.extend({
 		about: Ember.Route.extend({
 			route: '/about',
 			connectOutlets: function (router, context) {
-				router.get('applicationController').connectOutlet('about');
+				router.get('applicationController').connectOutlet('top', 'about');
+				router.get('aboutController').connectOutlet('sub', 'details')
 			},
 			goIndex: function (router, context) {
 				router.transitionTo('index');
@@ -38,5 +43,3 @@ Foss.Router = Ember.Router.extend({
 		})
 	})
 });
-
-Foss.initialize();
